6005d775721ad25a3c1f4e3ca65c7509567aa7bd
Niels <nvderv@hotmail.com>
2021-04-17 16:11:22 +0200

    Added function that will randomly return either "Rock", "Paper" or "Scissors"

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Test123");
+// Create a function called computerPlay that will randomly return either 'Rock', 'Paper' or 'Scissors'
+
+function computerPlay() {
+    let answers = ["Rock", "Paper", "Scissors"];
+    return answers[Math.floor(Math.random() * answers.length)];
+}
+
+// Write a function that plays a single round of Rock Paper Scissors. 
+
+// The function should take two parameters - the playerSelection and computerSelection 
+
+// Then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
