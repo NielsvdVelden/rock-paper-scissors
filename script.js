168a345f6cba1f3fc041da1159543f0bed37d8a7
Niels <nvderv@hotmail.com>
2021-04-20 17:02:59 +0200

    Added function to play a round of Rock-Paper-Scissors. Function takes user input and declares a winner based on the random computer selection.

--- script.js
+++ script.js
@@ -6,9 +6,34 @@ function computerPlay() {
 }
 
 // Write a function that plays a single round of Rock Paper Scissors. 
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "Paper") {
+        return ("You lose! Paper beats Rock");
+    } else if (playerSelection == "rock" && computerSelection == "Rock") {
+        return ("Tie!");
+    } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+        return ("You win! Rock beats Scissors");
+    } else if (playerSelection == "paper" && computerSelection == "Rock") {
+        return ("You win! Paper beats Rock");
+    } else if (playerSelection == "paper" && computerSelection == "Paper") {
+        return ("Tie!");
+    } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+        return ("You lose! Scissors beats Paper");
+    } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+        return ("You lose! Rock beats Scissors");
+    } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+        return ("You win! Scissors beats Paper");
+    } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
+        return ("Tie!");
+    } else {
+        return ("Please throw a valid handsign!");
+    }
+}
 
-// The function should take two parameters - the playerSelection and computerSelection 
-
-// Then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+// Testing
+let playerInput = prompt("Throw a handsign! \n (Rock, Paper or Scissors)");
+const playerSelection = playerInput.toLocaleLowerCase();
+const computerSelection = computerPlay();
+console.log ("Player selection: " + playerSelection);
+console.log("Computer selection: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
