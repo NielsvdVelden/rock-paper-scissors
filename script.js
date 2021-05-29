f43ad502aab312735669fe34baf8f11455cf4271
niels <nvderv@hotmail.com>
2021-05-29 14:01:56 +0200

    Cleaned up code.

--- script.js
+++ script.js
@@ -88,38 +88,3 @@ window.addEventListener("click", () => {
     playerWins = -9999;
   }
 });
-
-// // A function that uses the previous function to play a 5 round game that keeps score and reports a winner or loser at the end.
-
-// function game() {
-//   let gameNumber;
-//   for (gameNumber = 0; gameNumber < 6; gameNumber++) {
-//     let playerInput = prompt("Throw a handsign! \n (Rock, Paper or Scissors)");
-//     const playerSelection = playerInput.toLocaleLowerCase();
-//     const computerSelection = computerPlay();
-//     console.log("GAME " + gameNumber + ":");
-//     console.log("Player selection: " + playerSelection);
-//     console.log("Computer selection: " + computerSelection);
-//     console.log("\n");
-//     console.log(playRound(playerSelection, computerSelection));
-//     console.log("\n");
-
-//     let roundResult = playRound(playerSelection, computerSelection);
-
-//     if (roundResult.search("You win! ") > -1) {
-//       ++playerWins;
-//     } else if (roundResult.search("You lose! ") > -1) {
-//       ++computerWins;
-//     }
-
-//     console.log("Player wins: " + playerWins);
-//     console.log("Computer wins: " + computerWins);
-//     console.log("\n");
-
-//     if (playerWins >= 3 && computerWins < 2) {
-//       console.log("Game Over. You Win!");
-//     } else if (playerWins < 2 && computerWins >= 3) {
-//       console.log("Game Over. You Lose!");
-//     } else if (gameNumber >= 5 && playerWins < 3 && computerWins < 3) {
-//       console.log("It's a tie! Try again.");
-//     }
